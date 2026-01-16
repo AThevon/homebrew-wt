bdf9688979090d6ecc9e130e5d8f9edd0cb540b2
AThevon <adrienthevon@gmail.com>
2026-01-16 19:32:55 +0100

    Update wt to v1.1.2

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -4,8 +4,8 @@
 class Wt < Formula
   desc "Git worktree manager with fzf integration and GitHub PR support"
   homepage "https://github.com/AThevon/wt"
-  url "https://github.com/AThevon/wt/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "00f82144586807e02e6bddd0ec469b510bb7a780f228265168153912d25cf2f7"
+  url "https://github.com/AThevon/wt/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "4888b41a6332d72b39b4355f2ae8a5c47571e5a376ccbe5ab19d147347a71dfa"
   license "MIT"
 
   depends_on "fzf"
@@ -27,6 +27,6 @@ class Wt < Formula
   end
 
   test do
-    assert_match "wt 1.1.0", shell_output("#{bin}/wt-core --version 2>&1")
+    assert_match "wt 1.1.2", shell_output("#{bin}/wt-core --version 2>&1")
   end
 end
