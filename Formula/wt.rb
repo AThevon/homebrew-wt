c353048d7dfa263a4652a066965ea842a41ab3b4
AThevon <96548365+AThevon@users.noreply.github.com>
2026-02-23 13:30:24 +0100

    bump wt to v1.9.4

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -4,8 +4,8 @@
 class Wt < Formula
   desc "Git worktree manager with fzf integration and GitHub PR support"
   homepage "https://github.com/AThevon/wt"
-  url "https://github.com/AThevon/wt/archive/refs/tags/v1.9.3.tar.gz"
-  sha256 "e4cf78bd28ede75f10722b87dce5451e44a186a923065577470462c551fd7277"
+  url "https://github.com/AThevon/wt/archive/refs/tags/v1.9.4.tar.gz"
+  sha256 "0e01ce9bb95c3f9f3f26213020f9f2a8f75a59f7f53440a12e28360cd0858bb2"
   license "MIT"
 
   depends_on "fzf"
@@ -27,6 +27,6 @@ class Wt < Formula
   end
 
   test do
-    assert_match "wt 1.9.3", shell_output("#{bin}/wt-core --version 2>&1")
+    assert_match "wt 1.9.4", shell_output("#{bin}/wt-core --version 2>&1")
   end
 end
