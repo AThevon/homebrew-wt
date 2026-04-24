bdb77d7973ed6d682ec32fa2bf8da26ba51bb754
adri1 <96548365+AThevon@users.noreply.github.com>
2026-04-24 11:02:52 +0200

    Update wt to v2.2.0 (fix repo rename + install lib/assets + add gum/glab)

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -2,18 +2,23 @@
 # frozen_string_literal: true
 
 class Wt < Formula
-  desc "Git worktree manager with fzf integration and GitHub PR support"
-  homepage "https://github.com/AThevon/wt"
-  url "https://github.com/AThevon/wt/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "87cf1d27254afbf1478fb4bb3082f666567ccc7b7c7057cdd930c8b808fb2c4f"
+  desc "Git worktree manager with fzf integration and GitHub/GitLab support"
+  homepage "https://github.com/AThevon/worktigre"
+  url "https://github.com/AThevon/worktigre/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "57c27030be32ca8df4b2799a6fada4280bdbec076b7de5d152e94e51c419f683"
   license "MIT"
 
   depends_on "fzf"
   depends_on "gh"
+  depends_on "glab"
+  depends_on "gum"
   depends_on "jq"
 
   def install
     bin.install "wt.sh" => "wt-core"
+    (prefix/"lib/worktigre").install Dir["lib/*.sh"]
+    (prefix/"assets/worktigre").install Dir["assets/logo*.ansi"]
+    zsh_completion.install "completions/wt.zsh" => "_wt"
   end
 
   def caveats
@@ -27,6 +32,6 @@ class Wt < Formula
   end
 
   test do
-    assert_match "wt 2.0.1", shell_output("#{bin}/wt-core --version 2>&1")
+    assert_match "wt 2.2.0", shell_output("#{bin}/wt-core --version 2>&1")
   end
 end
